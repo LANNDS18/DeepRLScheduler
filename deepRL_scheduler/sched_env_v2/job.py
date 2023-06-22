dd940ac664d58f4031b53482c900ee646a5f0928
LANNDS18 <LANNDS18@icloud.com>
2023-06-22 17:46:12 +0100

    milestone1/feature v2 env is derived from deep batch scheduler, we can use it to efficiently run. However, this method lack of timestamp supporting the state space is smaller

---
+++ deepRL_scheduler/sched_env_v2/job.py
@@ -0,0 +1,86 @@
+import re
+
+
+class Job:
+    def __init__(self, line="0        0      0    0   0     0    0   0  0 0  0   0   0  0  0 0 0 0"):
+        line = line.strip()
+        s_array = re.split("\\s+", line)
+        self.id = int(s_array[0])
+        self.submit_time = int(s_array[1])
+        self.wait_time = int(s_array[2])
+        self.run_time = int(s_array[3])
+        self.number_of_allocated_processors = int(s_array[4])
+        self.average_cpu_time_used = float(s_array[5])
+        self.used_memory = int(s_array[6])
+
+        # "requested number of processors" and "number of allocated processors" are typically mixed.
+        # I do not know their difference clearly. But it seems to me using a larger one will be sufficient.
+        self.request_number_of_processors = int(s_array[7])
+        self.number_of_allocated_processors = max(self.number_of_allocated_processors,
+                                                  self.request_number_of_processors)
+        self.request_number_of_processors = self.number_of_allocated_processors
+
+        self.request_number_of_nodes = -1
+
+        # if we use the job's request time field
+        # for model, request_time might be empty. In this case, we set request_time to the run_time
+        self.request_time = int(s_array[8])
+        if self.request_time == -1:
+            self.request_time = self.run_time
+
+        # if we use the run time as the most accurate request time
+        # self.request_time = self.run_time + 60
+        # if we gradually increase the accuracy of job's request time
+        # with a percentage wrong estimation and round to a fixed time: 1,2,3,... hours.
+        # this.requestTime = (int) (this.runTime + this.runTime * 0.4);
+        # int roundsTo = 60 * 60; //round up to hours
+        # this.requestTime = (this.requestTime / roundsTo + 1) * roundsTo;
+
+        self.request_memory = int(s_array[9])
+        self.status = int(s_array[10])
+        self.user_id = int(s_array[11])
+        self.group_id = int(s_array[12])
+        self.executable_number = int(s_array[13])
+        self.queue_number = int(s_array[14])
+
+        try:
+            self.partition_number = int(s_array[15])
+        except ValueError:
+            self.partition_number = 0
+
+        self.proceeding_job_number = int(s_array[16])
+        self.think_time_from_proceeding_job = int(s_array[17])
+
+        self.random_id = self.submit_time
+
+        self.scheduled_time = -1
+
+        self.allocated_machines = None
+
+        self.slurm_in_queue_time = 0
+        self.slurm_age = 0
+        self.slurm_job_size = 0.0
+        self.slurm_fair = 0.0
+        self.slurm_partition = 0
+        self.slurm_qos = 0
+        self.slurm_tres_cpu = 0.0
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __lt__(self, other):
+        return self.id < other.id
+
+    def __str__(self):
+        return (
+            f'Job<{self.id}, {self.status}, start={self.submit_time}, '
+            f'processors={self.request_number_of_processors}, '
+            f'memory={self.request_memory} '
+            f'duration={self.run_time}>'
+        )
+
+
+    def __feature__(self):
+        return [self.submit_time, self.request_number_of_processors, self.request_time,
+                self.user_id, self.group_id, self.executable_number, self.queue_number]
+
---
+++ deepRL_scheduler/sched_env_v2/workload.py
@@ -0,0 +1,82 @@
+import sys
+
+from .job import Job
+
+
+class Workloads:
+
+    def __init__(self, path):
+        self.all_jobs = []
+        self.max = 0
+        self.max_exec_time = 0
+        self.min_exec_time = sys.maxsize
+        self.max_job_id = 0
+
+        self.max_requested_memory = 0
+        self.max_user_id = 0
+        self.max_group_id = 0
+        self.max_executable_number = 0
+        self.max_job_id = 0
+        self.max_nodes = 0
+        self.max_procs = 0
+
+        with open(path) as fp:
+            for line in fp:
+                if line.startswith(";"):
+                    if line.startswith("; MaxNodes:"):
+                        self.max_nodes = int(line.split(":")[1].strip())
+                    if line.startswith("; MaxProcs:"):
+                        self.max_procs = int(line.split(":")[1].strip())
+                    continue
+
+                j = Job(line)
+                if j.run_time > self.max_exec_time:
+                    self.max_exec_time = j.run_time
+                if j.run_time < self.min_exec_time:
+                    self.min_exec_time = j.run_time
+                if j.request_memory > self.max_requested_memory:
+                    self.max_requested_memory = j.request_memory
+                if j.user_id > self.max_user_id:
+                    self.max_user_id = j.user_id
+                if j.group_id > self.max_group_id:
+                    self.max_group_id = j.group_id
+                if j.executable_number > self.max_executable_number:
+                    self.max_executable_number = j.executable_number
+
+                # filter those illegal data whose runtime < 0
+                if j.run_time < 0:
+                    j.run_time = 10
+                if j.run_time > 0:
+                    self.all_jobs.append(j)
+
+                    if j.request_number_of_processors > self.max:
+                        self.max = j.request_number_of_processors
+
+        # if max_procs = 0, it means node/proc are the same.
+        if self.max_procs == 0:
+            self.max_procs = self.max_nodes
+
+        print("Max Allocated Processors:", str(self.max), ";max node:", self.max_nodes,
+              ";max procs:", self.max_procs,
+              ";max execution time:", self.max_exec_time)
+
+        self.all_jobs.sort(key=lambda job: job.id)
+
+    def size(self):
+        return len(self.all_jobs)
+
+    def reset(self):
+        for job in self.all_jobs:
+            job.scheduled_time = -1
+
+    def __getitem__(self, item):
+        return self.all_jobs[item]
+
+
+if __name__ == "__main__":
+    print("Loading the workloads...")
+    load = Workloads("../data/lublin_256.swf")
+    print("Finish loading the workloads...", type(load[0]))
+    print(load.max_nodes, load.max_procs)
+    print(load[0].__feature__())
+    print(load[1].__feature__())
